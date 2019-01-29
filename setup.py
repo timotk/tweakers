f43bff561c92c0989ca07832f1c784aec9c54e4e
Timo <timo_tk@hotmail.com>
2019-01-29 20:52:21 +0100

    Update version number to 3.2

--- setup.py
+++ setup.py
@@ -1,28 +1,28 @@
 from setuptools import setup
 
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read()
 
 
 setup(
-    name='tweakers',
+    name="tweakers",
     version="0.3.2",
-    description='A Python API for https://tweakers.net',
+    description="A Python API for https://tweakers.net",
     long_description=readme,
-    long_description_content_type='text/markdown',
-    url='https://github.com/timotk/tweakers',
-    packages=['tweakers'],
-    python_requires='>=3.6',
+    long_description_content_type="text/markdown",
+    url="https://github.com/timotk/tweakers",
+    packages=["tweakers"],
+    python_requires=">=3.6",
     install_requires=requirements,
-    test_requires=['pytest'],
-    license='MIT',
+    test_requires=["pytest"],
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ]
+        "Operating System :: OS Independent",
+    ],
 )
