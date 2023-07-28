d7fec649c54f63e05f7b1d19559df141ea4155bd
Timo <timo_tk@hotmail.com>
2023-07-28 19:05:42 +0200

    Update download script

--- scripts/download_test_files.py
+++ scripts/download_test_files.py
@@ -1,41 +1,16 @@
 from pathlib import Path
 import time
-import httpx
-from tenacity import (
-    retry,
-    stop_after_attempt,
-    wait_exponential,
-    retry_if_exception_type,
-)
-from tweakers.exceptions import RateLimitException
 
 from tweakers.utils import get
 
 URLS = {
     "topic": "https://gathering.tweakers.net/forum/list_messages/1908208",
-    "frontpage": "https://tweakers.net/", 
+    "frontpage": "https://tweakers.net/",
     "article": "https://tweakers.net/nieuws/212172/whatsapp-introduceert-functie-voor-korte-videoberichten.html",
     "user": "https://tweakers.net/gallery/1/",
     "active_topics": "https://gathering.tweakers.net/forum/list_activetopics",
+    "find": "https://tweakers.net/forum/find?keyword=playstation",
 }
-
-# @retry(
-#     retry=retry_if_exception_type(),
-#     wait=wait_exponential(multiplier=1, min=5, max=10),
-#     stop=stop_after_attempt(3),
-# )
-# def get(url):
-#     # TODO: Implement cookie?
-#     response = httpx.get(url)
-#     if response.status_code == 429:
-#         raise RateLimitException()
-    
-#     html = response.text
-    
-#     if "Sorry, je gaat even iets te snel" in html:
-#         raise Exception("Rate limited")
-    
-
 
 
 for name, url in URLS.items():
@@ -47,5 +22,3 @@ for name, url in URLS.items():
     path.parent.mkdir(parents=True, exist_ok=True)
     with path.open("w") as f:
         f.write(html)
-    time.sleep(10)
-
